67b681b9b93913803f403d789092e054fef99fb5
venuste-20002 <rukundovenu2019@gmail.com>
2024-08-19 17:09:22 +0200

    login

---
+++ src/routes/(dash)/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Disian from "../../components/Disian.svelte";
+    import Leftbar from "../../components/Leftbar.svelte";
+    import Rightbar from "../../components/Rightbar.svelte";
+</script>
+
+<div class="w-full min-h-screen flex bg-white">
+    <Leftbar />
+    <Rightbar>
+        <slot />
+    </Rightbar>
+</div>
---
+++ src/routes/(dash)/dash/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+  // Define the redirectTo function within the Svelte script block
+  function redirectTo(page) {
+    // Display a message asking the user to dial the USSD code.
+    // alert('Please dial *182# on your mobile device.');
+    
+    // Redirect to the desired page.
+    window.location.href = page;
+  }
+</script>
+
+<div class="absolute top-50 lg:left-[260px] flex flex-wrap z-50 gap-4 p-4">
+  <button
+    class="w-[19%] h-[150px] bg-white rounded-[10px] shadow-2xl p-4"
+    on:click={() => redirectTo('/money')}
+  >
+    MTN Mobile Money
+  </button>
+
+  <button
+    class="w-[19%] h-[150px] bg-white rounded-[10px] shadow-2xl p-4"
+    on:click={() => redirectTo('/airtime')}
+  >
+    MTN Airtime & Internet
+  </button>
+
+  <button
+    class="w-[19%] h-[150px] bg-white rounded-[10px] shadow-2xl p-4"
+    on:click={() => redirectTo('/airtel-money')}
+  >
+    Airtel Money
+  </button>
+
+  <button
+    class="w-[19%] h-[150px] bg-white rounded-[10px] shadow-2xl p-4"
+    on:click={() => redirectTo('/momo-pay')}
+  >
+    MoMoPay Merchant *182#
+  </button>
+
+  <button
+    class="w-[19%] h-[150px] bg-white rounded-[10px] shadow-2xl p-4"
+    on:click={() => redirectTo('/insurance')}
+  >
+    Insurance
+  </button>
+
+  <button
+    class="w-[19%] h-[150px] bg-white rounded-[10px] shadow-2xl p-4"
+    on:click={() => redirectTo('/canalbox-internet')}
+  >
+    CanalBox Internet
+  </button>
+
+  <button
+    class="w-[19%] h-[150px] bg-white rounded-[10px] shadow-2xl p-4"
+    on:click={() => redirectTo('/useful-codes')}
+  >
+    Rwanda Useful Codes
+  </button>
+</div>
