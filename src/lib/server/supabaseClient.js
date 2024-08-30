30c5c8b90900c02db89c5f0773f7f31a97499dab
kayigmb <kayigm1@gmail.com>
2024-08-30 12:32:05 +0200

    fix/auth

---
+++ src/lib/server/supabaseClient.js
@@ -0,0 +1,10 @@
+import {
+  PUBLIC_SUPABASE_ANON_KEY,
+  PUBLIC_SUPABASE_URL,
+} from "$env/static/public";
+import { createClient } from "@supabase/auth-helpers-sveltekit";
+
+export const supabase = createClient(
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY,
+);
